6e06374060aff3c4a03e0cf771d127bca2980325
Raihan Ali <m.raihanalifikri@gmail.com>
2024-07-30 13:38:56 +0700

    update

--- 7. OOP/implisit_class/Kerjaan/main.dart
+++ 7. OOP/implisit_class/Kerjaan/main.dart
@@ -1,4 +1,4 @@
-import '../../Inheritance/cat.dart';
+
 import 'animal.dart';
 import 'cat.dart';
 import 'dog.dart';
@@ -6,6 +6,16 @@ import 'fish.dart';
 
 
 void main() {
-  Animal animal = cat(nama, age, weight, color, eat, eyeColor);
-  person.greeting();
+  Cat animal1 = Cat("Moka", 3, "crem", "blue");
+  animal1.eat();
+  animal1.meow();
+
+  Dog animal2 = Dog("Dogi", 4, "black", "panjang");
+  animal2.eat();
+  animal2.woof();
+
+  Fish animal3 = Fish("Dori", 2, "blue", "bagus");
+  animal3.eat();
+  animal3.swim();
+
 }
