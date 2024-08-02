f622d41936e5a453f67f7f39ac1334bf345d3de5
Raihan Ali <m.raihanalifikri@gmail.com>
2024-08-02 09:30:33 +0700

    update

--- 7. OOP/function_style/high_order_function.dart
+++ 7. OOP/function_style/high_order_function.dart
@@ -1,11 +1,42 @@
+import 'dart:ffi';
+
 void main() {
-  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  // Mendefinisikan high order function dengan parameter fungsi
-  var cekNumber = (List numbers, Function(int cek)) {
-    for (var numbers in numbers) {
-      cek(numbers);
-    }
+  // Fungction High Order Yang menerima parameter fungsi
+  // Calculate sebagai nama fungsi
+  // Int a, int b sebagai parameter ke-1 ke-2
+  // Function Sebagai para meter ke-3
 
+  void calculate(int a, int b, Function operation) {
+    print("Result : ${operation(a, b)}");
   }
+   
+  //  Fungsi Penjumlahan
+  int add(int a, int b) => a + b;
+
+  // Fungsi Pengurangan 
+  int subtract(int a, int b) => a - b;
+
+  // Fungsi Pengurangan 
+  int kali(int a, int b) => a * b;
+
+  // Fungsi Pengurangan 
+  int bagi(int a, int b) => a % b;
+
+
+  // Memanggil fungsi high order
+  calculate(8, 7, add);
+
+  // Membuat function high order
+  void calculatingBall(double pi, double r, Function volume, Function luasPermukaan) {
+    print("Volume : ${volume(pi, r)}");
+    print("Luas Permukaan : ${luasPermukaan(pi ,r)}");
+  }
+
+  double volume(double pi, double r) => 4/3 * pi * r * r * r;
+
+  double luasPermukaan(double pi, double r) => 4 * pi * r * r ;
+
+  calculatingBall(3.14, 10, volume, luasPermukaan);
+
 }
